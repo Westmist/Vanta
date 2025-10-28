dc8a0be7c92c3650df62db4749003c7efb5c7d75
CongCong Liao <mingxuannull@gmail.com>
2025-10-28 21:34:29 +0800

    调整持久化池的初始时机

--- persistent-core/src/main/java/com/game/vanta/persistent/PersistentPool.java
+++ persistent-core/src/main/java/com/game/vanta/persistent/PersistentPool.java
@@ -7,18 +7,31 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Set;
 
-public class PersistentPool {
+public class PersistentPool implements SmartInitializingSingleton {
 
     private static final Logger log = LoggerFactory.getLogger(PersistentPool.class);
 
     private final BiMap<String, Class<? extends IPersistent>> persistentPool = HashBiMap.create();
 
+    private final PersistentProperties persistentProperties;
+
+    private final MongoTemplate mongoTemplate;
+
+    public PersistentPool(
+        PersistentProperties persistentProperties,
+        MongoTemplate mongoTemplate) {
+        this.persistentProperties = persistentProperties;
+        this.mongoTemplate = mongoTemplate;
+    }
+
     @SuppressWarnings("unchecked")
-    public PersistentPool(PersistentProperties persistentProperties, MongoTemplate mongoTemplate) {
+    @Override
+    public void afterSingletonsInstantiated() {
         String persistentEntityPackages = persistentProperties.getPersistentEntityPackages();
         if (persistentEntityPackages == null || persistentEntityPackages.isEmpty() || persistentEntityPackages.isBlank()) {
             log.warn("Persistent entity packages is not configured.");
